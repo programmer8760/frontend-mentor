7c354018723d0697dd7f8dc2ae8df0aef1602f15
programmer8760 <programmer8760@gmail.com>
2025-04-25 20:47:57 +0300

    feat: full challenge completion 🐄

--- blog-preview-card/src/App.jsx
+++ blog-preview-card/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import articleImg from './assets/images/illustration-article.svg';
+import avatar from './assets/images/image-avatar.webp';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='cardContainer'>
+     <div className='card'>
+        <div className='imageContainer'>
+          <img src={articleImg} className='articleImage' />
+        </div>
+        <div className='tagBar'>
+         <p className='tag'>Learning</p>
+        </div>
+        <p className='publishDate'>Published 21 Dec 2023</p>
+        <p className='articleTitle'>HTML & CSS foundations</p>
+        <p className='articleDescription'>These languages are the backbone of every website, defining structure, content, and presentation.</p>
+        <div className='author'>
+          <img src={avatar} className='authorAvatar' />
+          <p className='authorName'>Greg Hooper</p>
+        </div>
+     </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
